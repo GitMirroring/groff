GitMirroring/groff

commit 77d8f728dba1c483b2fbbbe64017c0668c5b8450

[troff]: Trivially refactor.
Rename `charinfo` class's member function `get_unicode_code()` to
`get_unicode_mapping()`.

* src/roff/troff/charinfo.h (class charinfo): Do it.

* src/roff/troff/input.cpp (charinfo::get_unicode_code): Rename this...
  (charinfo::get_unicode_mapping): ...to this.

  (define_class, charinfo::get_flags): Update call sites.

src/roff/troff/input.cpp
@@ -8233,8 +8233,8 @@ void define_class()
 	skip_line();
 	return;
       }
-      int u1 = child1->get_unicode_code();
-      int u2 = child2->get_unicode_code();
+      int u1 = child1->get_unicode_mapping();
+      int u2 = child2->get_unicode_mapping();
       if (u1 < 0) {
 	warning(WARN_SYNTAX,
 		"invalid start value in character range");
@@ -8260,7 +8260,7 @@ void define_class()
 	ci->add_to_class(child1);
       }
       else {
-	int u1 = child1->get_unicode_code();
+	int u1 = child1->get_unicode_mapping();
 	if (u1 < 0) {
 	  warning(WARN_SYNTAX,
 		  "invalid character value in class '%1'",
@@ -8290,7 +8290,7 @@ void define_class()
       ci->add_to_class(child1);
     }
     else {
-      int u1 = child1->get_unicode_code();
+      int u1 = child1->get_unicode_mapping();
       if (u1 < 0) {
 	warning(WARN_SYNTAX,
 		"invalid character value in class '%1'",
@@ -10198,7 +10198,7 @@ charinfo::charinfo(symbol s)
   get_flags();
 }
 
-int charinfo::get_unicode_code()
+int charinfo::get_unicode_mapping()
 {
   if (ascii_code != 0U)
     return ascii_code;
@@ -10251,7 +10251,7 @@ void charinfo::get_flags()
   // troff's dictionary types use a pre-STL approach to containers.
   while (iter.get(&s, reinterpret_cast<void **>(&ci))) {
     assert(!s.is_null());
-    if (ci->contains(get_unicode_code())) {
+    if (ci->contains(get_unicode_mapping() >= 0)) {
 #if defined(DEBUGGING)
       if (want_html_debugging)
 	fprintf(stderr, "charinfo::get_flags %p %s %d\n",

src/roff/troff/charinfo.h
@@ -88,7 +88,7 @@ public:
   unsigned char get_hyphenation_code();
   unsigned char get_ascii_code();
   unsigned char get_asciify_code();
-  int get_unicode_code();
+  int get_unicode_mapping();
   void set_hyphenation_code(unsigned char);
   void set_ascii_code(unsigned char);
   void set_asciify_code(unsigned char);

commit c545d5192dcf7beb643c8ce362fa3b17fcf1d033

[troff]: Fix Savannah #67570.
* src/roff/troff/input.cpp (charinfo::get_flags): Fix inadvertent
  regression caused by fumbled refactoring, wherein I inexplicably
  mistook a Boolean-valued return type for an `int`-valued one.
  (`contains()` sounds Boolean to me now, in the cold light of day.)

Fixes <https://savannah.gnu.org/bugs/?67570>.  Thanks to Dave Kemper for
the report.  Problem introduced by me in commit 77d8f728d, 4 September.

src/roff/troff/input.cpp
@@ -10731,7 +10731,7 @@ void charinfo::get_flags()
   // troff's dictionary types use a pre-STL approach to containers.
   while (iter.get(&s, reinterpret_cast<void **>(&ci))) {
     assert(!s.is_null());
-    if (ci->contains(get_unicode_mapping() >= 0)) {
+    if (ci->contains(get_unicode_mapping())) {
 #if defined(DEBUGGING)
       if (want_html_debugging)
 	fprintf(stderr, "charinfo::get_flags %p %s %d\n",