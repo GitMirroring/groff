46e62c53d3710075174d08af1f73f67f5407f1e7
G. Branden Robinson <g.branden.robinson@gmail.com>
2025-12-13 12:01:20 -0600

    [grog]: Fix Savannah #67802.
    * src/utils/grog/grog.pl: Improve heuristics for distinguishing mm(7)
      documents from those for other packages.  (interpret_line): Recognize
      `APP`, `APPSK`, `AF`, `AS`, `COVEND`, `INITI`, `IND`, `INDP`, `INITR`,
      `SETR`, `GETHN`, `GETPN`, `ISODATE`, `MULB`, `MULN`, and `MULE` as
      unique to mm.  (infer_man_or_ms_package): Stop recognizing `RP`, `TL`,
      `AU`, `ND`, `AE`, `B1`, `B2`, `DS`, `DE`, `1C`, `2C`, and `MC` as
      unique to ms, since mm also defines them.  Stop recognizing `P` and
      `EX` as unique to man, since mm also defines them.
    
    * src/utils/grog/tests/smoke-test.sh: Add most mm examples as test
      cases.  A couple are deeply ambiguous, like "B1B2", which is so
      limited that it is valid (and renders sensibly) with either the mm(7)
      _or_ ms(7) packages, and "story.mm" which uses only macro names that
      are shared variously among man(7), mm(7), mom(7), and ms(7).
    
    Fixes <https://savannah.gnu.org/bugs/?67802>.

--- src/utils/grog/tests/smoke-test.sh
+++ src/utils/grog/tests/smoke-test.sh
@@ -69,10 +69,19 @@ echo "testing grn(1)- and eqn(1)-using me(7) document $doc" >&2
 "$grog" "$doc" | \
     grep -Fqx 'groff -e -g -me '"$doc"
 
-doc=$src/contrib/mm/examples/letter.mm
-echo "testing mm(7) document $doc" >&2
+for f in APP COVER IND LT ML MOVE MUL NCOL ND References SETR \
+         letter.mm memorandum.mm
+do
+    doc=$src/contrib/mm/examples/$f
+    echo "testing mm(7) document $doc" >&2
+    "$grog" "$doc" | \
+        grep -Fqx 'groff -mm '"$doc"
+done
+
+doc=$src/contrib/mm/examples/LT.se
+echo "testing mmse(7) document $doc" >&2
 "$grog" "$doc" | \
-    grep -Fqx 'groff -mm '"$doc"
+    grep -Fqx 'groff -mmse '"$doc"
 
 doc=$src/contrib/mom/examples/copyright-chapter.mom
 echo "testing mom(7) document $doc" >&2
