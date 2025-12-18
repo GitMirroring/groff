60840028a7520215babd775010976de9d90aa91f
G. Branden Robinson <g.branden.robinson@gmail.com>
2025-12-18 13:14:07 -0600

    Reduce the Bennett Brauer factor (cosmetic) (1/4).
    Thus named because 'excessive' use of 'quotation marks' in 'ordinary
    prose' gouges my eyeballs like Chris Farley's fingers making air quotes
    a millimeter from my face.

--- src/utils/grog/grog.pl
+++ src/utils/grog/grog.pl
@@ -8,14 +8,14 @@
 # Rewritten in Perl by Bernd Warken <groff-bernd.warken-72@web.de>.
 # Hacked up by G. Branden Robinson, 2021.
 
-# This file is part of 'grog', which is part of 'groff'.
+# This file is part of 'grog', which is part of groff.
 
-# 'groff' is free software; you can redistribute it and/or modify it
+# groff is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
 
-# 'groff' is distributed in the hope that it will be useful, but
+# groff is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # General Public License for more details.
