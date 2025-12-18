397b40acf7b9a80a830923fd4a88f0b8c4ebb6b3
G. Branden Robinson <g.branden.robinson@gmail.com>
2025-12-18 13:30:41 -0600

    Reduce the Bennett Brauer factor (cosmetic) (3/4).
    chem is "distributed with", not "part of", groff.

--- contrib/chem/chem.pl
+++ contrib/chem/chem.pl
@@ -6,7 +6,9 @@ my $copyright = 'Copyright (C) 2006-2014, 2022'
                 . ' Free Software Foundation, Inc.';
 # Written by Bernd Warken <groff-bernd.warken-72@web.de>.
 
-# This file is part of 'chem', which is part of groff.
+# This file is part of chem.
+#
+# chem is distributed with groff, the GNU roff typesetting system.
 
 # groff is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License (GPL) version 2 as
