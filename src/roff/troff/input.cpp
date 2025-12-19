GitMirroring/groff

commit 1fb72c15e678a41faffed6e3888e5818e50becb4

[troff]: Improve description of node tokens.
* src/roff/troff/input.cpp (token::description): Describe a `TOKEN_NODE`
  explicitly as a "token", because it still is one.

Continues commit dd7bf97938, 23 November.

src/roff/troff/input.cpp
@@ -2986,7 +2986,8 @@ const char *token::description()
       static char nodebuf[bufsz];
       (void) strcpy(nodebuf, "an undescribed node");
       describe_node(nodebuf, bufsz);
-      return nodebuf;
+      (void) snprintf(buf, bufsz, "%s token", nodebuf);
+      return buf;
     }
   case TOKEN_INDEXED_CHAR:
     (void) snprintf(buf, maxstr, "indexed character %d",

commit c3421fdc8c6f5ceeccca5de6281e9a029420318a

src/roff/troff/input.cpp: Size a buffer better.
* src/roff/troff/input.cpp (token::description): Size the buffer housing
  the node description more scrupulously, avoiding potential string
  truncation.

Fixes problem introduced by me in commit 1fb72c15e6, 3 December.

../src/roff/troff/input.cpp: In member function ‘const char* token::description()’:
../src/roff/troff/input.cpp:3025:38: warning: ‘ token’ directive output may be truncated writing 6 bytes into a region of size between 1 and 47 [-Wformat-truncation=]
 3025 |       (void) snprintf(buf, bufsz, "%s token", nodebuf);
      |                                      ^~~~~~
../src/roff/troff/input.cpp:3025:22: note: ‘snprintf’ output between 7 and 53 bytes into a destination of size 47
 3025 |       (void) snprintf(buf, bufsz, "%s token", nodebuf);
      |              ~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

src/roff/troff/input.cpp
@@ -3019,7 +3019,7 @@ const char *token::description()
     return "a newline";
   case TOKEN_NODE:
     {
-      static char nodebuf[bufsz];
+      static char nodebuf[bufsz - (sizeof " token")];
       (void) strcpy(nodebuf, "an undescribed node");
       describe_node(nodebuf, bufsz);
       (void) snprintf(buf, bufsz, "%s token", nodebuf);