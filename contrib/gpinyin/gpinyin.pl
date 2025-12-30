ac991fffd87ef2aa255637e49155a3f1dc5baa21
G. Branden Robinson <g.branden.robinson@gmail.com>
2025-12-30 02:49:13 -0600

    [gpinyin]: Rationalize copyright licensing.
    Bernd Warken stopped contributing to groff development ten years ago.
    Six years before that, groff updated its use of the GPL from version 2
    to version 3.  I don't know if Bernd neglected to update the license
    notices in his gpinyin files from v2 to v3, or deliberately stuck with
    v2.  I assume the latter, because he omitted the "or any later version"
    phrasing that the FSF encourages GPL users to employ.
    
    At the same time, Bernd's choice is not binding on the copyrightable
    contributions of others, and groff has been distributed under GNU GPLv3
    since 2009, a fact that cannot have escaped him.
    
    Consequently, I'll be adding a copy of GPLv2 to contrib/gpinyin to
    reflect Bernd's work, but retaining GPLv3-oriented license notices
    corresponding to all work on it since then, which has mostly been done
    by GNU maintainers of groff--Werner Lemberg, Bertrand Garrigues, and me.
    
    $ git shortlog -s contrib/gpinyin/
        16  Bernd Warken
         6  Bertrand Garrigues
         1  Colin Watson
        77  G. Branden Robinson
         1  Ville Skyttä
         5  Werner Lemberg
    
    I leave as an exercise for the GPLv3-phobic the extraction of Bernd's
    GPLv2-licensed work from the current gpinyin source configuration,
    should anyone be interested in doing so.

--- contrib/gpinyin/gpinyin.pl
+++ contrib/gpinyin/gpinyin.pl
@@ -12,20 +12,18 @@
 #
 # gpinyin is distributed with groff, the GNU roff typesetting system.
 #
-# gpinyin is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
+# groff is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
 #
-# 'gpinyin' is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+# groff is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
 #
-# You can find a copy of the GNU General Public License in the internet
-# at <http://www.gnu.org/licenses/gpl-2.0.html>.
-
-########################################################################
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 use strict;
 use warnings;
