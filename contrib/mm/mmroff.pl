a20d7c1dfc075eac2258773322dfc683ec2211a8
G. Branden Robinson <g.branden.robinson@gmail.com>
2025-12-30 03:23:41 -0600

    [mm]: Update license notices.
    Reflow notices to fit within 72 columns.
    
    Apply notice terms to "groff", not to "mm" per se, as "mm" is
    _distributed_ with groff under the GPLv3 being a "contrib" component.
    
    Update URLs to use HTTPS scheme rather than HTTP.
    
    Also add bug reporting instructions from m.tmac to mmroff.pl and
    refer-mm.tmac.

--- contrib/mm/mmroff.pl
+++ contrib/mm/mmroff.pl
@@ -8,8 +8,8 @@
 #
 # groff is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
 #
 # groff is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -18,6 +18,9 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Please submit bug reports using groff's 'BUG-REPORT' file to
+# http://savannah.gnu.org/bugs/?group=groff.
 
 use strict;
 use warnings;
