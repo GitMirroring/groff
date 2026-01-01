GitMirroring/groff

commit 1740c22b4a97b473b25ea68f70ee5fab341bfda6

[preconv]: Fix code style nits (5/6).
* src/preproc/preconv/preconv.cpp (conversion_iconv, do_file): Favor C++
  `static_cast<>` and `const_cast<>` operators over omnipotent C-style
  casts.

Some pretty hairy C-style casts while looping over the input and output
buffers (to resize them if necessary) remain.  Migrating those will
demand more careful thought.

src/preproc/preconv/preconv.cpp
@@ -701,8 +701,8 @@ conversion_iconv(FILE *fp, const string &data, char *enc)
   char *limit;
   while (inbytes_left > 0) {
     size_t status = iconv(handle,
-			  (ICONV_CONST char **)&inptr, &inbytes_left,
-			  &outptr, &outbytes_left);
+			  static_cast<ICONV_CONST char **>(&inptr),
+			  &inbytes_left, &outptr, &outbytes_left);
     if (status == static_cast<size_t>(-1)) {
       if (EILSEQ == errno) {
 	// Invalid byte sequence.  XXX
@@ -735,8 +735,8 @@ conversion_iconv(FILE *fp, const string &data, char *enc)
     inbytes_left += read_bytes;
     while (inbytes_left > 0) {
       size_t status = iconv(handle,
-			    (ICONV_CONST char **)&inptr, &inbytes_left,
-			    &outptr, &outbytes_left);
+			    static_cast<ICONV_CONST char **>(&inptr),
+			    &inbytes_left, &outptr, &outbytes_left);
       if (status == (size_t)-1) {
 	if (EILSEQ == errno) {
 	  // Invalid byte sequence.  XXX
@@ -1154,12 +1154,12 @@ do_file(const char *filename)
 	fprintf(stderr, "  but BOM in data stream implies encoding"
 		" '%s'!\n", BOM_encoding);
     }
-    encoding = (char *)user_encoding;
+    encoding = static_cast<char *>(user_encoding);
   }
   else if (BOM_encoding != 0 /* nullptr */) {
     if (is_debugging)
       fprintf(stderr, "  found BOM, no search for coding tag\n");
-    encoding = (char *)BOM_encoding;
+    encoding = const_cast<char *>(BOM_encoding);
   }
   else {
     // 'check_coding_tag' returns a pointer to a static array (or a null

commit e8d21654c16acae268c15372f280bc5131ebab05

[preconv]: Fix improper type cast.
* src/preproc/preconv/preconv.cpp (conversion_iconv): Use `const_cast<>`
  to cast away `const`, not `static_cast<>`.  Fixes build on Solaris
  11/sparc64/GCC 5.5.  Problem introduced by me in commit 1740c22b4a, 11
  July.  Thanks to Bruno Haible for the clue.

src/preproc/preconv/preconv.cpp
@@ -702,7 +702,7 @@ conversion_iconv(FILE *fp, const string &data, char *enc)
   char *limit;
   while (inbytes_left > 0) {
     size_t status = iconv(handle,
-			  static_cast<ICONV_CONST char **>(&inptr),
+			  const_cast<ICONV_CONST char **>(&inptr),
 			  &inbytes_left, &outptr, &outbytes_left);
     if (status == static_cast<size_t>(-1)) {
       if (EILSEQ == errno) {
@@ -737,7 +737,7 @@ conversion_iconv(FILE *fp, const string &data, char *enc)
     inbytes_left += read_bytes;
     while (inbytes_left > 0) {
       size_t status = iconv(handle,
-			    static_cast<ICONV_CONST char **>(&inptr),
+			    const_cast<ICONV_CONST char **>(&inptr),
 			    &inbytes_left, &outptr, &outbytes_left);
       if (status == (size_t)-1) {
 	if (EILSEQ == errno) {