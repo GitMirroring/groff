30a81fbc3c711644caeac791ad5c843bda38e7df
G. Branden Robinson <g.branden.robinson@gmail.com>
2026-04-14 06:21:28 -0500

    [groff]: Unit-test `dt` request.
    * src/roff/groff/tests/dt-request-works.sh: Do it.
    
    * src/roff/groff/groff.am (groff_TESTS): Run test.

---
+++ src/roff/groff/tests/dt-request-works.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#
+# Copyright 2026 G. Branden Robinson
+#
+# This file is part of groff, the GNU roff typesetting system.
+#
+# groff is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# groff is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+groff="${abs_top_builddir:-.}/test-groff"
+
+# Unit-test `dt` request.
+
+input='.
+.de TT
+BOING
+.br
+..
+.di DD
+.dt 3v TT
+.nf
+foo
+bar
+.sp
+qux
+.di
+.DD
+.'
+
+output=$(printf '%s\n' "$input" | "$groff" -a 2>/dev/null)
+echo "$output"
+output=$(echo $output) # condense onto one line
+echo "$output" | grep -q "bar BOING qux"
+
+# vim:set autoindent expandtab shiftwidth=4 tabstop=4 textwidth=72:
