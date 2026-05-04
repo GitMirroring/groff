GitMirroring/groff

commit ca8c376830e8a59a7ca504e6b4845ed528e83066

[troff]: More requests warn of missing arguments.
[troff]: Make more requests that take mandatory arguments--specifically
`pn`, `ti`, `rchar`, and `hpfcode`--throw warning diagnostics in
category "missing" when they aren't given any.

* src/roff/troff/div.cpp (page_number):
* src/roff/troff/env.cpp (temporary_indent):
* src/roff/troff/input.cpp (define_special_character)
  (hyphenation_patterns_file_code): Do it.

src/roff/troff/env.cpp
@@ -1566,11 +1566,20 @@ void indent()
 void temporary_indent()
 {
   bool is_valid = true;
-  hunits temp;
-  if (!get_hunits(&temp, 'm', curenv->get_indent()))
-    is_valid = false;
-  while (!tok.is_newline() && !tok.is_eof())
-    tok.next();
+  hunits temp = H0;
+  if (!has_arg()) {
+    warning(WARN_MISSING, "temporary indentation request expects"
+	    " argument");
+    skip_line();
+    // _Don't_ return early; when invoked with the ordinary control
+    // character this request still breaks the line.
+  }
+  else {
+    if (!get_hunits(&temp, 'm', curenv->get_indent()))
+      is_valid = false;
+    while (!tok.is_newline() && !tok.is_eof())
+      tok.next();
+  }
   if (want_break)
     curenv->do_break();
   if (temp < H0) {

src/roff/troff/div.cpp
@@ -885,6 +885,12 @@ void need_space()
 
 void page_number()
 {
+  if (!has_arg()) {
+    warning(WARN_MISSING, "page number assignment request expects an"
+	    " argument");
+    skip_line();
+    return;
+  }
   int n = 0;
   // the ps4html register is set if we are using -Tps
   // to generate images for html

src/roff/troff/input.cpp
@@ -4573,6 +4573,12 @@ void define_special_character()
 static void remove_character()
 {
   tok.skip();
+  if (!has_arg()) {
+    warning(WARN_MISSING, "character definition removal request expects"
+	    " arguments");
+    skip_line();
+    return;
+  }
   while (!tok.is_newline() && !tok.is_eof()) {
     if (!tok.is_space() && !tok.is_tab()) {
       charinfo *ci = tok.get_char(true /* required */);
@@ -7879,6 +7885,12 @@ void hyphenation_patterns_file_code()
   error("hyphenation pattern file code assignment request will be"
 	" withdrawn in a future groff release; migrate to 'hcode'");
   tok.skip();
+  if (!has_arg()) {
+    warning(WARN_MISSING, "hyphenation pattern file code assignment"
+	    " request expects arguments");
+    skip_line();
+    return;
+  }
   while (!tok.is_newline() && !tok.is_eof()) {
     int n1, n2;
     if (get_integer(&n1) && (0 <= n1 && n1 <= 255)) {

commit 962f25ee8557b10f7f197616f9df85a46d9cabdd

[troff]: Fix Savannah #68285.
* src/roff/troff/env.cpp (temporary_indent): When invalidly given no
  argument, the `ti` request was failing to correctly skip the remainder
  of the input line.  Drop two instances of logic that "manually" pump
  the input stream by calling `tok.next()`.  The second is better
  replaced by `skip_line()`, and the first made redundant thereby.
  Nearly all request handlers call that function to consume the
  remainder of the input line once they've handled all the arguments
  they expect.

Fixes <https://savannah.gnu.org/bugs/?68285>.  Problem introduced by me
in commit ca8c376830, 2024-11-09.  I altered request handlers for four
requests in that commit: `pn`, `ti`, `rchar`, and `hpfcode`, but
fortunately made this mistake only with `ti`.

src/roff/troff/env.cpp
@@ -1701,9 +1701,6 @@ void temporary_indent() // .ti
     }
     if (!read_hunits(&temp, 'm', curenv->get_indent()))
       is_valid = false;
-    // XXX: Why not `skip_line()`?
-    while (!tok.is_terminator())
-      tok.next();
   }
   if (was_invoked_with_regular_control_character)
     curenv->do_break();
@@ -1717,7 +1714,7 @@ void temporary_indent() // .ti
     curenv->have_temporary_indent = true;
     curdiv->modified_tag.incl(MTSM_TI);
   }
-  tok.next(); // XXX: Why not `skip_line()`?
+  skip_line();
 }
 
 void configure_underlining(bool want_spaces_underlined)