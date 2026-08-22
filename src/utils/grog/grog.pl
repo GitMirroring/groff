GitMirroring/groff

commit b161e50e93f126b9c8aa194b2d85728b5b5ad508

[grog]: Add undocumented debugging mode.
* src/utils/grog/grog.pl: New global scalar `debugging` of Boolean sense
  tracks debugging state.  Initialize false.

  (debug): New subroutine emits diagnostic message only if
  debugging is enabled.

  (process_arguments): Check for `--debug` command-line option.

  (interpret_line, infer_man_or_ms_package, [main]): Report debugging
information of interest.

Feature prompted by surprising diagnosis of Lesk's "Notes on the IBM C
Compiler." memo as a _me_(7) document instead of the more correct
_ms_(7).  The misdiagnosis appears to be due to the inadvertent
downcasing of some _ms_(7) macro calls, `IP` and `TP` in the source
document.  (There are other problems with the document, including a
fascinating crime against the CSTR #49 specification of _tbl_ input that
no contemporary implementation supports, and perhaps only Lesk himself
knew about, being that program's author.) See
<https://www.tuhs.org/pipermail/tuhs/2026-July/034010.html> ff.

src/utils/grog/grog.pl
@@ -66,6 +66,7 @@ my $have_seen_first_macro_call = 0;
 # man(7) and ms(7) use many of the same macro names; do extra checking.
 my $man_score = 0;
 my $ms_score = 0;
+my $debugging = 0;
 
 my $had_inference_problem = 0;
 my $had_processing_problem = 0;
@@ -82,6 +83,14 @@ sub fail {
 sub gripe {
   my $text = shift;
   print STDERR "$program_name: warning: $text\n";
+}
+
+
+sub debug {
+  if ($debugging) {
+    my $text = shift;
+    print STDERR "$program_name: debug: $text\n";
+  }
 }
 
 
@@ -140,7 +149,9 @@ sub process_arguments {
     version() if ($arg eq '-v' || $arg eq '--version');
     usage(0) if ($arg eq '-h' || $arg eq '--help');
 
-    if ($arg =~ '^--.') {
+    if ($arg eq '--debug') {
+      $debugging = 1;
+    } elsif ($arg =~ '^--.') {
       fail("unrecognized grog option '$arg'; ignored");
       usage(1);
       next;
@@ -216,6 +227,7 @@ sub interpret_line {
   # is redefined, we could delete it from the relevant lists and
   # hashes.
   if ($line =~ /\\\" Automatically generated by Pod::Man/) {
+    debug("Pod::Man document detected: juicing \$man_score");
     $man_score += 100;
   }
 
@@ -350,6 +362,7 @@ sub interpret_line {
   # What remains must be a macro name.
   my $macro = $command;
 
+  debug("incrementing \$score{$macro}");
   $score{$macro}++;
 
 
@@ -363,6 +376,7 @@ sub interpret_line {
   # We can put one thumb on the scale, however.
   if ((!$have_seen_first_macro_call) && ($macro eq 'TH')) {
     # TH as the first call in a document screams man(7).
+    debug("first macro call is 'TH': juicing \$man_score");
     $man_score += 100;
   }
 
@@ -561,6 +575,9 @@ sub infer_man_or_ms_package {
     $ms_score += $score{$key};
   }
 
+  debug("\$man_score = $man_score");
+  debug("\$ms_score = $ms_score");
+
   if (!$ms_score && !$man_score) {
     # The input may be a "raw" roff document; this is not a problem,
     # but it does mean no package was inferred.
@@ -710,7 +727,12 @@ read_input();
 
 if ($have_any_valid_operands) {
   infer_preprocessors();
-  infer_man_or_ms_package() if (scalar @inferred_main_package != 1);
+  if (scalar @inferred_main_package != 1) {
+    infer_man_or_ms_package();
+  } else {
+    debug("skipping man vs. ms disambiguation; already inferred package"
+	  . " '@inferred_main_package'");
+  }
   construct_command();
 }
 

commit f6caa62c1aa6a0d8ad2843931640673aead3d355

[grog]: Fix Savannah #68635.
* src/utils/grog/grog.pl (process_arguments): Relocate test for
  `--debug` option such that we don't interpolate it into the argument
  list we build for groff, which won't recognize it.

Fixes <https://savannah.gnu.org/bugs/?68635>.  Problem introduced by me
in commit b161e50e93, 2026-07-17.

src/utils/grog/grog.pl
@@ -112,6 +112,11 @@ sub process_arguments {
       next;
     }
 
+    if ($arg eq '--debug') {
+      $debugging = 1;
+      next; # Don't pass the option to groff.
+    }
+
     if ($delayed_option) {
       if ($delayed_option eq '-m') {
 	push @requested_package, $arg;
@@ -149,9 +154,7 @@ sub process_arguments {
     version() if ($arg eq '-v' || $arg eq '--version');
     usage(0) if ($arg eq '-h' || $arg eq '--help');
 
-    if ($arg eq '--debug') {
-      $debugging = 1;
-    } elsif ($arg =~ '^--.') {
+    if ($arg =~ '^--.') {
       fail("unrecognized grog option '$arg'; ignored");
       usage(1);
       next;