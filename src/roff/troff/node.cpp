GitMirroring/groff

commit cdb50249e92622a7f34e04b180d80059847b1edd

[troff]: Implement recursive node dumping (5m/9).
* src/roff/troff/node.h (class device_extension_node): Specialize
  {override} `dump_properties()` for this class.

* src/roff/troff/node.cpp (device_extension_node::dump_properties): New
  member function reports values of `tfont`, `stroke_color`, and
  `fill_color` properties.

Changes `pline` request output as follows.

-{"type": "device_extension_node", "diversion level": 0, "is_special_node": true},
+{"type": "device_extension_node", "diversion level": 0, "is_special_node": true, "tfont": "TR", "stroke_color": "default", "fill_color": "default"},

src/roff/troff/node.cpp
@@ -4073,6 +4073,19 @@ device_extension_node::device_extension_node(const macro &m, tfont *t,
 {
 }
 
+void device_extension_node::dump_properties()
+{
+  node::dump_properties();
+  // TODO: Implement `macro::dump()` and call it on `mac` from here.
+  fputs(", \"tfont\": ", stderr);
+  tf->get_name().json_dump();
+  fputs(", \"stroke_color\": ", stderr);
+  gcol->nm.json_dump();
+  fputs(", \"fill_color\": ", stderr);
+  fcol->nm.json_dump();
+  fflush(stderr);
+}
+
 bool device_extension_node::is_same_as(node *n)
 {
   return ((mac == static_cast<device_extension_node *>(n)->mac)

src/roff/troff/node.h
@@ -594,6 +594,7 @@ public:
   bool is_tag();
   int ends_sentence();
   tfont *get_tfont();
+  void dump_properties();
 };
 
 class suppress_node : public node {

commit b7d51a93d6e1b32e71bc60be04921f2cdb56dd8d

[troff]: Fix Savannah #68642.
* src/roff/troff/node.cpp (device_extension_node::dump_properties): Wrap
  JSON output of object of type `macro` with braces since it has a
  composite nature.

Fixes <https://savannah.gnu.org/bugs/?68642>.  Problem introduced by me
in commit cdb50249e9, 2025-03-03.

src/roff/troff/node.cpp
@@ -4410,8 +4410,9 @@ device_extension_node::device_extension_node(const macro &m, tfont *t,
 void device_extension_node::dump_properties()
 {
   node::dump_properties();
-  fputs(", \"macro\": ", stderr);
+  fputs(", \"macro\": {", stderr);
   mac.json_dump();
+  fputc('}', stderr);
   fputs(", \"tfont\": ", stderr);
   tf->get_name().json_dump();
   fputs(", \"stroke_color\": ", stderr);