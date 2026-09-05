5cbf279b2c39a16856c2eb53463ed0764a9d4948
G. Branden Robinson <g.branden.robinson@gmail.com>
2026-09-04 20:26:11 -0500

    [groff]: Fix Savannah #68676.
    * src/roff/groff/tests/pdf-device-smoke-test.sh: Rewrite test script to
      check only GNU troff's "grout" output; stop attempting to validate
      contents of PDF generated by gropdf(1).  That task is best left to
      scripts in src/devices/gropdf/tests.
    
    Fixes <https://savannah.gnu.org/bugs/?68676>.

--- src/roff/groff/tests/pdf-device-smoke-test.sh
+++ src/roff/groff/tests/pdf-device-smoke-test.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 #
-# Copyright 2025 G. Branden Robinson
-#
 # This file is part of groff, the GNU roff typesetting system.
 #
 # groff is free software; you can redistribute it and/or modify it under
@@ -19,15 +17,11 @@
 
 groff="${abs_top_builddir:-.}/test-groff"
 
-# Expected output:
-#0000460   H   e   l   l   o   ,  nl   q  sp   B   T  nl   1  sp   0  sp
-#0001020   H   1   0   6   3   4   0  nl   %  sp   t   o   r   l   d   !
-
-# '-P -d' because we want the PDF uncompressed for pattern matching.
-output=$(echo "Hello, world!" | "$groff" -T pdf -P -d | od -t a)
-echo "$output" | grep '^0000460'
-echo "$output" | grep '^0001020'
-echo "$output" | grep -Eq '^0000460.*H +e +l +l +o +,' || exit 1
-echo "$output" | grep -Eq '^0001020.*o +r +l +d +!'    || exit 1
+output=$(echo "Hello, world!" | "$groff" -T pdf -Z)
+echo "$output"
+echo "$output" | grep -Eqx 'x +T +pdf' || exit 1
+# TODO: Make following regexes stricter when Savannah #63544 is fixed.
+echo "$output" | grep -Eqx 't *Hello,' || exit 1
+echo "$output" | grep -Eqx 't *orld!' || exit 1
 
 # vim:set autoindent expandtab shiftwidth=4 tabstop=4 textwidth=72:
